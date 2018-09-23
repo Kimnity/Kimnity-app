08d36da0f78fb1b5289d4566fbf774f35bbcd2ae
nishi <pisces.nisikawa@gmail.com>
2018-09-23 19:19:14 +0900

    ログイン機能実装

--- api/app/controllers/api/auth/registrations_controller.rb
+++ api/app/controllers/api/auth/registrations_controller.rb
@@ -1,16 +1,17 @@
-module Auth
+module Api
+  module Auth
 
-  class Api::Auth::RegistrationsController < ApplicationController
-    private
+    class RegistrationsController < DeviseTokenAuth::RegistrationsController
+      private
 
-    def sign_up_params
-      params.permit(:name, :email, :password, :password_confirmation)
+      def sign_up_params
+        params.permit(:email, :password, :password_confirmation)
+      end
+
+      def account_update_params
+        params.permit(:name, :email)
+      end
+
     end
-
-    def account_update_params
-      params.permit(:name, :email)
-    end
-
   end
-
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     mount_devise_token_auth_for 'User', at: 'auth',controllers: {
-      registrations: 'auth/registrations'
+      registrations: 'api/auth/registrations'
     }
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
