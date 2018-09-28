22ddcc33d5d982c987c3ee6a761b0bc9263c1d21
nishi <pisces.nisikawa@gmail.com>
2018-09-28 20:19:19 +0900

    postテーブル作成

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_22_115832) do
+ActiveRecord::Schema.define(version: 2018_09_28_081147) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "content", null: false
+    t.integer "total_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider", default: "email", null: false
