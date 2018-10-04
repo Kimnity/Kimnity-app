62b3e9ad91167350a1c00f4af549e8610d56ca93
mo-ro <gainmoro@gmail.com>
2018-10-04 08:13:28 -0700

    はいいけた。次はログイン

--- front/index.js
+++ front/index.js
@@ -7,7 +7,7 @@ Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
       component: {
-        name: 'SignUp'
+        name: 'Initializing'
       }
     },
   });
--- front/src/navigation.js
+++ front/src/navigation.js
@@ -1,37 +1,9 @@
 import { Navigation } from 'react-native-navigation';
 
-console.log(6789)
-
 export const goToAuth = () => Navigation.setRoot({
   root: {
-    bottomTabs: {
-      id: 'BottomTabsId',
-      children: [
-        // {
-        //   component: {
-        //     name: 'SignIn',
-        //     options: {
-        //       bottomTab: {
-        //         fontSize: 12,
-        //         text: 'Sign In',
-        //         // icon: require('./signin.png')
-        //       }
-        //     }
-        //   },
-        // },
-        {
-          component: {
-            name: 'SignUp',
-            options: {
-              bottomTab: {
-                text: 'Sign Up',
-                fontSize: 12,
-                // icon: require('./signup.png')
-              }
-            }
-          },
-        },
-      ],
+    component: {
+      name: 'SignUp'
     }
   }
 });
